6cedac99fc683b817a64cf5d1d3bcf097bde07b1
Arun kumar <arunk83534@gmail.com>
2025-06-20 12:09:59 -0500

    chore: update description

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -8,7 +8,7 @@
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
-#
+
 #
 ################################
 
