46e7e3646fa753f270c66a52210a5894089de43e
Ubuntu <ubuntu@ip-172-31-0-11.ap-southeast-2.compute.internal>
2025-06-20 06:08:43 +0000

    Updated Test

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -10,10 +10,10 @@
 #   Please provide your github token and rest api to the script as input
 #
 #
+#
 ################################
 
-if [ ${#@} -lt 2 ]; then
-    echo "usage: $0 [your github token] [REST expression]"
+if [ ${#@} -lt 2 ]; then echo "usage: $0 [your github token] [REST expression]"
     exit 1;
 fi
 
